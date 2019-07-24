3bc83d2124b6d630230fa0933906c8940fa4d9d6
Erik Grosskurth <egrosskurth@Erik-Grosskurths-MacBook-Pro.local>
2019-07-24 14:15:43 -0400

    Adding CORS and updating for npm security vulnerabilities

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Mongoose = require('mongoose');
 const Bluebird = require('bluebird');
+const CORS = require('cors');
 const { connStr } = require('./config/creds');
 const { log } = require('./utils/logging');
 const { storeConnection } = require('./utils/connection');
@@ -13,8 +14,9 @@ const apiPrefix = '/comingSoon/v1';
 Mongoose.Promise = Bluebird;
 Mongoose.connect(connStr, { useNewUrlParser: true });
 Mongoose.connection.once('open', () => {
-    storeConnection(Mongoose.connection, 'wga')
+    storeConnection(Mongoose.connection, 'wga');
     app.use(log);
+    app.use(CORS());
     app.get(`${apiPrefix}/content`, log, getContent);
     app.listen(port);
     console.log(`App is listening on ${ port }`);
